b4116e3c03caf7a2c43ae43f1fe28898b258ec41
robin2017 <robin.seu@foxmail.com>
2019-03-05 01:29:22 +0800

    完成全部的导航框架功能(后期需要总结优化)

--- pages/PopularPage.js
+++ pages/PopularPage.js
@@ -15,23 +15,58 @@ import {
     createMaterialTopTabNavigator
 } from 'react-navigation'
 import NavigationUtil from '../js/navigator/NavigationUtil'
+import DynamicTabNavigator from "../js/navigator/DynamicTabNavigator";
 
 export default class PopularPage extends Component {
-    render() {
-        const TabNavigator = createAppContainer(createMaterialTopTabNavigator({
-            PopularTab1: {
-                screen: PopularTab,
+    constructor(props) {
+        super(props);
+        //动态配置tabs名称
+        this.tabNames = ['JAVA', 'ANDROID', 'IOS', 'REACT', 'REACT NATIVE', 'PHP']
+
+    }
+
+    /**
+     PopularTab1: {
+        screen: PopularTab,
+        navigationOptions: {
+            title: 'Tab1'
+        }
+    },
+     PopularTab2: {
+        screen: PopularTab,
+        navigationOptions: {
+            title: 'Tab2'
+        }
+    }*/
+    //类似于DynamicTabNavigator的TABS，具体格式见上
+    _getTabs() {
+        const tabs = {};
+        this.tabNames.forEach((item, index) => {
+            tabs[`tab${index}`] = {
+                screen: props =><PopularTab {...props} tabLabel={item}/>,
                 navigationOptions: {
-                    title: 'Tab1'
-                }
-            },
-            PopularTab2: {
-                screen: PopularTab,
-                navigationOptions: {
-                    title: 'Tab2'
+                    title: item,
                 }
             }
-        }));
+        });
+        return tabs;
+    }
+
+    render() {
+        const TabNavigator = createAppContainer(createMaterialTopTabNavigator(
+            this._getTabs(), {
+                tabBarOptions: {
+                    tabStyle: styles.tabStyle,
+                    upperCaseLabel: false,
+                    scrollEnabled: true,//可以滚动，很重要属性
+                    style: {
+                        backgroundColor: '#678'
+                    },
+                    indicatorStyle: styles.indicatorStyle,//标签指示器样式
+                    labelStyle: styles.labelStyle
+                }
+            }
+        ));
         return <View style={{flex: 1, marginTop: 0}}>
             <TabNavigator/>
         </View>
@@ -61,5 +96,17 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#F5FCFF',
+    },
+    tabStyle: {
+        minWidth: 50
+    },
+    indicatorStyle: {
+        height: 2,
+        backgroundColor: 'white'
+    },
+    labelStyle: {
+        fontSize: 13,
+        marginTop: 6,
+        marginBottom: 6
     }
 });
