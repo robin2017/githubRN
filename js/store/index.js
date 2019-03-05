acecb186fab5c78f5fd9ba39858407a3ce0f7b03
robin2017 <robin.seu@foxmail.com>
2019-03-05 21:51:18 +0800

    添加logger中间件，记录redux日志

--- js/store/index.js
+++ js/store/index.js
@@ -3,8 +3,20 @@ import thunk from 'redux-thunk'
 import reducers from '../reducer'
 import {middleware} from '../navigator/AppNavigator'
 
+const logger = store =>next=>action=>{
+    if(typeof action==='function'){
+        console.log("logger:dispatching a function")
+    }else {
+        console.log("logger:dispatching ",action)
+    }
+    const result = next(action)
+    console.log('logger:nextState ',store.getState())
+}
+
+
 const middlewares = [
     middleware,
+    logger,
     thunk,
 ];
 
