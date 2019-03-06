aa245dc94e9bd5e308c24f4d36cf157a2c1cac8a
robin2017 <robin.seu@foxmail.com>
2019-03-06 19:47:43 +0800

    物理返回键成功

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -12,16 +12,39 @@ import {
 
 import NavigationUtil from '../js/navigator/NavigationUtil'
 import DynamicTabNavigator from '../js/navigator/DynamicTabNavigator'
-export default class HomePage extends Component {
 
+import {BackHandler} from 'react-native'
+import {NavigationActions} from 'react-navigation'
+import {connect} from "react-redux";
+
+
+class HomePage extends Component {
+    componentDidMount() {
+        BackHandler.addEventListener('hardwareBackPress', this.onBackPress);
+    }
+
+    componentWillUnmount() {
+        BackHandler.removeEventListener('hardwareBackPress', this.onBackPress);
+    }
+
+    onBackPress = () => {
+        const {dispatch, nav} = this.props;
+        //if (nav.index === 0) {
+        if (nav.routes[1].index === 0) {//如果RootNavigator中的MainNavigator的index为0，则不处理返回事件
+            return false;
+        }
+        dispatch(NavigationActions.back());
+        return true;
+    };
 
     render() {
         //储存外层navigation
         NavigationUtil.navigation = this.props.navigation
-        console.log('保存时：',NavigationUtil.navigation)
+        console.log('保存时：', NavigationUtil.navigation)
         return <DynamicTabNavigator/>
     }
 }
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -30,3 +53,12 @@ const styles = StyleSheet.create({
         backgroundColor: '#F5FCFF',
     }
 });
+
+
+//订阅
+const mapStateToProps = (state) => ({
+    nav: state.nav
+});
+
+
+export default connect(mapStateToProps,)(HomePage)
